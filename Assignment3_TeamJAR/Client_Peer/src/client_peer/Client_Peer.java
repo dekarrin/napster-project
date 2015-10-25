b9d4e04dbd27062020af2069421d9f94d9cb3f5c
obsidiandart <john.schutz@ndsu.edu>
2015-10-25 15:33:08 -0500

    fixed project setup

---
+++ Assignment3_TeamJAR/Client_Peer/src/client_peer/Client_Peer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client_peer;
+
+/**
+ *
+ * @author joschutz
+ */
+public class Client_Peer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
--- Assignment3_TeamJAR/Server/src/server/Server.java
+++ Assignment3_TeamJAR/Server/src/server/Server.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package assignment3_teamjar;
+package server;
 
 /**
  *
  * @author joschutz
  */
-public class Assignment3_TeamJAR {
+public class Server {
 
     /**
      * @param args the command line arguments
