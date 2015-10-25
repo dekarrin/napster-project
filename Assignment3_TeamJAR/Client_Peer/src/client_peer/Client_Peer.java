f4377c485351d1c5586d6c00d2d75c1c69d96aa5
Rebecca 'TF' Nelson <tails.fanatic@gmail.com>
2015-10-25 15:37:26 -0500

    c/p'd files

--- Assignment3_TeamJAR/Client_Peer/src/client_peer/Client_Peer.java
+++ Assignment3_TeamJAR/Client_Peer/src/client_peer/Client_Peer.java
@@ -15,7 +15,57 @@ public class Client_Peer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // accept ip and port of server, shared_directory from command line
+
+
+        // connect to server at server-ip on server-port
+        // create empty map for downloaded files
+        // spawn (client_heartbeat_thread)
+        // spawn (peer_server)
+        // send list of files
+        // while connected:
+                // get user input
+                // if user input is download:
+                        // prompt for file to download
+                        // send search to server
+                        // if search contains peers:
+                                // prompt for peer
+                                // spawn thread (download_from_peer)
+                // if user input is status:
+                        // display whether that file is downloaded
+                // if user input is exit:
+                        // connected is false
+        // close server connection
+
+
+        // (peer_server)
+        // create TCP socket
+        // while true:
+                // accept connection
+                // spawn (upload_to_peer)
+                // close connection
+
+
+        // (upload_to_peer)
+        // open file
+        // transmit file
+
+
+        // (download_from_peer)
+        // add file to downloaded files map, set to false
+        // open tcp connection to peer
+        // download file
+        // close tcp connection to peer
+        // tell server to update file list
+        // set file to true in downloaded files
+
+
+        // (client_heartbeat_thread)
+        // connect to UDP server
+        // while connected:
+                // block for 60 seconds
+                // send heartbeat
+        // close heartbeat connection
     }
     
 }
--- Assignment3_TeamJAR/Server/src/server/Server.java
+++ Assignment3_TeamJAR/Server/src/server/Server.java
@@ -15,7 +15,36 @@ public class Server {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // accepts a port from the command line
+
+        // open TCP socket on port, listen for connection from client
+        // when client connects, spawn new thread to handle it and do (new_client_thread)
+
+
+        // (new_client_thread):
+        // spawn thread for heartbeat (heartbeat_thread)
+        // add client to whatever list/map/structure is keeping track of clients
+        // accept list of files from client
+        // while client is not dropped:
+                // wait for input from client
+                // if input is search:
+                        // return a list of peers that have file
+                // elif input is update:
+                        // now peer has that file
+        // close socket
+
+
+        // (heartbeat_thread):
+        // create new UDP socket for heartbeat
+        // establish connection with client
+        // while client is not dropped:
+                // check for message from client
+                // if there is a message:
+                        // reset time, loop
+                // if not check time, if greater than 200 seconds:
+                        // drop client
+        // close socket
     }
     
 }
