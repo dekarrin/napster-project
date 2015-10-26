f44ce85c973c5e270702ba97c94c6b04bc0b6f44
obsidiandart <john.schutz@ndsu.edu>
2015-10-25 19:05:25 -0500

    many changes to client classes

--- Assignment3_TeamJAR/Client_Peer/src/client_peer/Client_Peer.java
+++ Assignment3_TeamJAR/Client_Peer/src/client_peer/Client_Peer.java
@@ -1,28 +1,145 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package client_peer;
 
-/**
- *
- * @author joschutz
- */
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
 public class Client_Peer {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+     static void main(String[] args) {
+	 
         // accept ip and port of server, shared_directory from command line
+	 if (args.length < 3){
+	     System.err.println("Must have server IP, server port, and folder of shared files.");
+	     System.exit(1);
+	 }
+	 try{
+	     String serverIP = args[0];
+	     int serverPort = Integer.parseInt(args[1]);
+	     String filePath = args[2];
+	     Client_Peer clpr = new Client_Peer(serverIP, serverPort, filePath);
+	     clpr.run();	
+	 } catch (Exception e){
+	     System.err.println(e);
+	 }
+     }
+     
+     private String serverIP;
+     private int serverPort;
+     private String folderPath;
+     final private static int peerPort = 4004;
 
-
-        // connect to server at server-ip on server-port
-        // create empty map for downloaded files
-        // spawn (client_heartbeat_thread)
+     public Client_Peer(String ip, int port, String path){
+	 serverIP = ip;
+	 serverPort = port;
+	 folderPath = path;
+     }
+	 
+     public void run() {
+	 
+	 Socket socket = null;
+	 
+	 // connect to server at server-ip on server-port
+	 try{
+	    socket = new Socket(InetAddress.getByName(serverIP), serverPort);
+	 }catch(Exception e){
+	     e.printStackTrace();
+	     System.exit(1);
+	 } 
+	 
+	 // create empty map for downloaded files
+	 Map<String, Boolean> downloadedFiles = new HashMap<>();
+	 
+	 BufferedReader in = null;
+	 int heartPort = -1;
+	 
+	 try{
+	     in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	     heartPort = Integer.parseInt(in.readLine());
+	 }catch (Exception e){
+	     e.printStackTrace();
+	     System.exit(1);
+	 }
+	 
+	 // spawn (client_heartbeat_thread)
+	 try{
+	    new Client_Heartbeat_Thread(InetAddress.getByName(serverIP), heartPort).start();
+	 }catch (Exception e){
+	     e.printStackTrace();
+	     System.exit(1);
+	 }	 
+	 
+        
+        
         // spawn (peer_server)
-        // send list of files
+        
+	 
+	 try{
+	     File directory = new File(folderPath);
+	     File[] fList = directory.listFiles();
+	 
+	     DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+	 
+	     // send list of files
+	     out.write(fList.length);
+	     out.writeBytes("\n");
+	     for(int i = 0; i<fList.length; i++){
+	         out.writeBytes(fList[i].getName());
+		 out.writeBytes("\n");
+	     }
+	 
+	 
+	     while(socket.isConnected()){
+		 
+		 System.out.println("Download, Status, Exit");
+		 Scanner scan = new Scanner(System.in);
+		 
+		 String userInput = scan.nextLine();
+		 
+		 if(userInput.equalsIgnoreCase("download")){
+		     System.out.println("What is the name of the file you are searching for?");
+		     String searchFile = scan.nextLine();
+		     out.writeBytes("Search\n");
+		     out.writeBytes(searchFile);
+		     out.writeBytes("\n");
+		     while(!in.ready()){
+			 Thread.sleep(10);
+		     }
+		     String ips = in.readLine();
+		     if(ips.equals("")){
+			 System.out.println("The file you are searching for doesn't exist.");
+		     }else{
+			String[] ipList = ips.split(",");
+			System.out.println("Select the number of the IP you wish to download from.");
+			for(int i = 0; i<ipList.length; i++){
+			    System.out.println(i + " => " + ipList[i]);
+			}
+			userInput = scan.nextLine();
+			try{
+			    int userInt = Integer.parseInt(userInput);
+			    new Download_Thread(InetAddress.getByName(ipList[userInt]), peerPort, searchFile).start();
+			}catch(Exception e){
+			    e.printStackTrace();
+			}
+		     }
+		 }else if (userInput.equalsIgnoreCase("status")){
+		     //something
+		 }else if (userInput.equalsIgnoreCase("exit")){
+		     break;
+		 }
+		 
+	    }
+	     
+	     in.close();
+	     out.close();
+	     socket.close();
+	 
+	 
+	 }catch (Exception e){
+	     e.printStackTrace();
+	     System.exit(1);
+	 }
+	
         // while connected:
                 // get user input
                 // if user input is download:
@@ -36,6 +153,7 @@ public class Client_Peer {
                 // if user input is exit:
                         // connected is false
         // close server connection
+     }
 
 
         // (peer_server)
@@ -60,12 +178,7 @@ public class Client_Peer {
         // set file to true in downloaded files
 
 
-        // (client_heartbeat_thread)
-        // connect to UDP server
-        // while connected:
-                // block for 60 seconds
-                // send heartbeat
-        // close heartbeat connection
-    }
+        
+    
     
 }
